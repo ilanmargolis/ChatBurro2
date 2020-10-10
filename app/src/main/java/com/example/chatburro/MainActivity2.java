0bd4510e6d1b1cd288521754c94ac9d52cbca6a8
Ilan Margolis <ilanmargolis@gmail.com>
2020-10-09 21:10:51 -0300

    Atulaização da ActivityMain2

--- app/src/main/java/com/example/chatburro/MainActivity2.java
+++ app/src/main/java/com/example/chatburro/MainActivity2.java
@@ -1,21 +1,14 @@
 package com.example.chatburro;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.support.wearable.activity.WearableActivity;
-import android.widget.TextView;
 
-public class MainActivity2 extends WearableActivity {
-
-    private TextView mTextView;
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
-        mTextView = (TextView) findViewById(R.id.text);
-
-        // Enables Always-on
-        setAmbientEnabled();
     }
 }
