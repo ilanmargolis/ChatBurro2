dc28faed0bcf81e9b7939a602e34e75842802208
Ilan Margolis <ilanmargolis@gmail.com>
2020-10-14 18:08:25 -0300

    Atualizando conversa entre as Activitys

--- app/src/main/java/com/example/chatburro/MainActivity1.java
+++ app/src/main/java/com/example/chatburro/MainActivity1.java
@@ -1,5 +1,6 @@
 package com.example.chatburro;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -14,26 +15,39 @@ import static android.widget.Toast.*;
 
 public class MainActivity1 extends AppCompatActivity {
 
-    EditText editText;
+    TextView tvMsg1;
+    EditText etMsg1;
+    Button btEnviar1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
 
-        Button button = findViewById(R.id.button);
-        TextView textView = findViewById(R.id.textView);
+        tvMsg1 = (TextView) findViewById(R.id.tvMsg1);
+        btEnviar1 = (Button) findViewById(R.id.btEnviar1);
+        etMsg1 = (EditText) findViewById(R.id.etMsg1);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        btEnviar1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editText = findViewById(R.id.editText);
-                String mensagem = editText.getText().toString();
-                Intent intent = new Intent(MainActivity1.this, MainActivity2.class);
-                intent.putExtra("MENSAGEM", mensagem);
+                String mensagem = etMsg1.getText().toString();
 
-                startActivity(intent);
+                Intent intent = new Intent(MainActivity1.this, MainActivity2.class);
+                intent.putExtra("MENSAGEM1", mensagem);
+
+                startActivityForResult(intent, 90);
+
+                etMsg1.setText("");
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 90 && resultCode == RESULT_OK) {
+            tvMsg1.setText(data.getStringExtra("MENSAGEM2"));
+        }
     }
 }
--- app/src/main/java/com/example/chatburro/MainActivity2.java
+++ app/src/main/java/com/example/chatburro/MainActivity2.java
@@ -2,18 +2,48 @@ package com.example.chatburro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    TextView tvMsg2;
+    EditText etMsg2;
+    Button btEnviar2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        String mensagem = getIntent().getStringExtra("MENSAGEM");
+        tvMsg2 = (TextView) findViewById(R.id.tvMsg2);
+        btEnviar2 = (Button) findViewById(R.id.btEnviar2);
+        etMsg2 = (EditText) findViewById(R.id.etMsg2);
 
-        
+        String mensagem = getIntent().getStringExtra("MENSAGEM1");
+        tvMsg2.setText(mensagem);
+        etMsg2.setText("");
+
+        btEnviar2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mensagem = etMsg2.getText().toString();
+
+                Bundle res = new Bundle();
+                res.putString("MENSAGEM2", mensagem);
+
+                Intent i = new Intent();
+                i.putExtras(res);
+
+                setResult(RESULT_OK, i);
+
+                finish();
+            }
+        });
     }
 }
